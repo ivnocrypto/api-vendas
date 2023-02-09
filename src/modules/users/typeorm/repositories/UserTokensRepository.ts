9e146e01cda33337ec148e86ad44d88e62e021d5
ivnogg <ivnocrypto@gmail.com>
2023-02-09 18:20:48 -0300

    Repositório de Tokens de Usuários, criado método generate

--- src/modules/users/typeorm/repositories/UserTokensRepository.ts
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -4,33 +4,23 @@ import UserToken from '../entities/UserToken';
 @EntityRepository(UserToken)
 class UserTokensRepository extends Repository<UserToken> {
   public async findByToken(token: string): Promise<UserToken | undefined> {
-    const user = await this.findOne({
-      where: {
-        token,
-      },
-    });
-
-    return user;
-  }
-
-  public async findById(id: string): Promise<UserToken | undefined> {
     const userToken = await this.findOne({
       where: {
-        id,
+        token,
       },
     });
 
     return userToken;
   }
 
-  public async findByEmail(email: string): Promise<UserToken | undefined> {
-    const user = await this.findOne({
-      where: {
-        email,
-      },
+  public async generate(user_id: string): Promise<UserToken | undefined> {
+    const userToken = await this.create({
+      user_id,
     });
 
-    return user;
+    await this.save(userToken);
+
+    return userToken;
   }
 }
 
