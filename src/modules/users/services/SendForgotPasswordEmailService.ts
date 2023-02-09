f7c091908f0c17c4f3ddb7a0f4b1e29e8b46cbe1
ivnogg <ivnocrypto@gmail.com>
2023-02-09 20:03:49 -0300

    Serviço de Envio de Email para Recuperação de Senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,32 +1,25 @@
 import AppError from '@shared/errors/AppError';
-import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
-import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
 
 interface IRequest {
   email: string;
 }
 
 class SendForgotPasswordEmailService {
-  public async execute({ email }: IRequest): Promise<User> {
+  public async execute({ email }: IRequest): Promise<void> {
     const usersRepository = getCustomRepository(UsersRepository);
-    const emailExists = await usersRepository.findByEmail(email);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
 
-    if (emailExists) {
-      throw new AppError('Email address already used.');
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists');
     }
 
-    const hashedPassword = await hash(password, 8);
-
-    const user = usersRepository.create({
-      email,
-      password: hashedPassword,
-    });
-
-    await usersRepository.save(user);
-
-    return user;
+    const token = await userTokensRepository.generate(user.id);
+    console.log(token);
   }
 }
 
