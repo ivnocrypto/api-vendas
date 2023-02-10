b2aa4f5e67486b3f48d30c399fc8a1f9549f7a1c
ivnogg <ivnocrypto@gmail.com>
2023-02-10 09:27:54 -0300

    Instalado date-fns

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,29 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+    const userToken = await userTokensRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User Token does not exists');
+    }
+    const user = await usersRepository.findById(userToken.user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists');
+    }
+  }
+}
+
+export default ResetPasswordService;
