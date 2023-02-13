bc98adaa9bbd735e941e644504a2c85b66d77bc5
ivnogg <ivnocrypto@gmail.com>
2023-02-13 01:04:16 -0300

    Instalado date-fs

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -1,7 +1,5 @@
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
-import { hash } from 'bcryptjs';
-import { isAfter, addHours } from 'date-fns';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import UserTokensRepository from '../typeorm/repositories/UsersTokensRepository';
 
@@ -20,20 +18,8 @@ class ResetPasswordService {
     if (!userToken) {
       throw new AppError('User Token does not exists');
     }
+
     const user = await usersRepository.findById(userToken.user_id);
-
-    if (!user) {
-      throw new AppError('User does not exists');
-    }
-
-    const tokenCreatedAt = userToken.created_at;
-    const compareDate = addHours(tokenCreatedAt, 2);
-
-    if (isAfter(Date.now(), compareDate)) {
-      throw new AppError('Token expired.');
-    }
-
-    user.password = await hash(password, 8);
   }
 }
 
