d5fa57c45dfd2e7f4e399068bd15a73dd79c278d
ivnogg <ivnocrypto@gmail.com>
2023-02-19 18:52:43 -0300

    Criada tabela AddCustomerIdToOrders

--- src/shared/typeorm/migrations/1676840592094-AddCustomerIdToOrders.ts
+++ src/shared/typeorm/migrations/1676840592094-AddCustomerIdToOrders.ts
@@ -1,11 +1,34 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
 
 export class AddCustomerIdToOrders1676840592094 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders',
+      new TableColumn({
+        name: 'customer_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+    await queryRunner.createForeignKey(
+      'orders',
+      new TableForeignKey({
+        name: 'OrdersCustomer',
+        columnNames: ['customer_id'],
+        referencedTableName: 'customers',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders', 'OrderCustomer');
+    await queryRunner.dropColumn('orders', 'customer_id');
+  }
 }
