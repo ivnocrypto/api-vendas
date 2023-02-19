3a8e7e4aa563aa5ec0c6ca1b68bcfc1bb3c796ee
ivnogg <ivnocrypto@gmail.com>
2023-02-19 18:00:38 -0300

    Criada tabela CreateOrders

--- src/shared/typeorm/migrations/1676839974465-CreateOrders.ts
+++ src/shared/typeorm/migrations/1676839974465-CreateOrders.ts
@@ -1,11 +1,35 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateOrders1676839974465 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders');
+  }
 }
