be891541587f3ea2dadf1a9f472239ee1c05e670
ivnogg <ivnocrypto@gmail.com>
2023-02-19 20:04:41 -0300

    Criada migration AddProductIdToOrdersProducts

---
+++ src/shared/typeorm/migrations/1676847828416-AddProductIdToOrdersProducts.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddProductIdToOrdersProducts1676847828416 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
