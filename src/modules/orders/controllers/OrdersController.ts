ecf43124a1ad10352c18a8405884b247c1ced7f2
ivnogg <ivnocrypto@gmail.com>
2023-02-21 20:12:50 -0300

    OrderController finalizado

--- src/modules/orders/controllers/OrdersController.ts
+++ src/modules/orders/controllers/OrdersController.ts
@@ -1,66 +1,28 @@
 import { Request, Response } from 'express';
-import CreateProductService from '../services/CreateProductService';
-import ListProductService from '../services/ListProductService';
-import ShowProductService from '../services/ShowProductService';
-import UpdateProductService from '../services/UpdateProductService';
-import DeleteProductService from '../services/DeleteProductService';
+import CreateOrderService from '../services/CreateOrderService';
+import ShowOrderService from '../services/ShowOrderService';
 
-export default class ProductController {
-  public async index(request: Request, response: Response): Promise<Response> {
-    const listProducts = new ListProductService();
-
-    const products = await listProducts.execute();
-
-    return response.json(products);
-  }
-
+export default class ProductsController {
   public async show(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
 
-    const showProduct = new ShowProductService();
+    const showOrder = new ShowOrderService();
 
-    const product = await showProduct.execute({ id });
+    const order = await showOrder.execute({ id });
 
-    return response.json(product);
+    return response.json(order);
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
-    const { name, price, quantity } = request.body;
+    const { customer_id, products } = request.body;
 
-    const createProduct = new CreateProductService();
+    const createOrder = new CreateOrderService();
 
-    const product = await createProduct.execute({
-      name,
-      price,
-      quantity,
+    const order = await createOrder.execute({
+      customer_id,
+      products,
     });
 
-    return response.json(product);
-  }
-
-  public async update(request: Request, response: Response): Promise<Response> {
-    const { name, price, quantity } = request.body;
-    const { id } = request.params;
-
-    const updateProduct = new UpdateProductService();
-
-    const product = await updateProduct.execute({
-      id,
-      name,
-      price,
-      quantity,
-    });
-
-    return response.json(product);
-  }
-
-  public async delete(request: Request, response: Response): Promise<Response> {
-    const { id } = request.params;
-
-    const deleteProduct = new DeleteProductService();
-
-    await deleteProduct.execute({ id });
-
-    return response.json([]);
+    return response.json(order);
   }
 }
