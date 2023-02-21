cdc5264d45968acc60eb55fe9d564284761b609c
ivnogg <ivnocrypto@gmail.com>
2023-02-21 12:59:00 -0300

    Terminado relacionamento entre entidades Order, OrderProduct e Product

--- src/modules/orders/typeorm/entities/OrdersProducts.ts
+++ src/modules/orders/typeorm/entities/OrdersProducts.ts
@@ -8,6 +8,7 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 import Order from './Order';
+import Product from '@modules/products/typeorm/entities/Product';
 
 @Entity('orders_products')
 class OrdersProducts {
@@ -17,6 +18,10 @@ class OrdersProducts {
   @ManyToOne(() => Order, order => order.order_products)
   @JoinColumn({ name: 'order_id' })
   order: Order;
+
+  @ManyToOne(() => Product, product => product.order_products)
+  @JoinColumn({ name: 'product_id' })
+  product: Product;
 
   @Column('decimal')
   price: number;
