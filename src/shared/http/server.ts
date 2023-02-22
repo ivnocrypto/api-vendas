83872621c81b39cbfc457bed23aae0ba087ca197
ivnogg <ivnocrypto@gmail.com>
2023-02-22 08:52:04 -0300

    Middleware pagination adicionado

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -13,13 +13,15 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(pagination);
+
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use(errors());
 
 app.use(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({
@@ -27,7 +29,6 @@ app.use(
         message: error.message,
       });
     }
-    // eslint-disable-next-line no-console
     console.log(error);
 
     return response.status(500).json({
