e41df1cde621ba2fe565efe757b83ed921a5ba4e
ivnogg <ivnocrypto@gmail.com>
2023-02-22 23:49:19 -0300

    docker exec -it redis sh

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -7,7 +7,11 @@ class ListProductService {
   public async execute(): Promise<Product[]> {
     const productsRepository = getCustomRepository(ProductRepository);
 
+    const redisCache = new RedisCache();
+
     const products = productsRepository.find();
+
+    await redisCache.save('teste', 'teste');
 
     return products;
   }
