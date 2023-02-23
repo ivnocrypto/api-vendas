3bf089063d7eff84b1d87af47c0ed525768984fb
ivnogg <ivnocrypto@gmail.com>
2023-02-22 22:17:59 -0300

    import RedisCache ListProductService

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,6 +1,7 @@
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
 import ProductRepository from '../typeorm/repositories/ProductsRepository';
+import RedisCache from '@shared/cache/RedisCache';
 
 class ListProductService {
   public async execute(): Promise<Product[]> {
