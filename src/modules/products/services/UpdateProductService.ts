22718893d91ee02647c9fe60d60ca25c297bfda3
ivnogg <ivnocrypto@gmail.com>
2023-02-23 01:34:28 -0300

    import redisCache in UpdateProductService

--- src/modules/products/services/UpdateProductService.ts
+++ src/modules/products/services/UpdateProductService.ts
@@ -1,3 +1,4 @@
+import RedisCache from '@shared/cache/RedisCache';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
