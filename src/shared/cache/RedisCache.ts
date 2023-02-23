3a4f1842fbb1abd2f8f07418d5060f49cf7eb604
ivnogg <ivnocrypto@gmail.com>
2023-02-22 21:28:20 -0300

    Estrutura Classe RedisCache.ts

--- src/shared/cache/RedisCache.ts
+++ src/shared/cache/RedisCache.ts
@@ -7,4 +7,12 @@ export default class RedisCache {
   constructor() {
     this.client = new Redis(cacheConfig.config.redis);
   }
+
+  public async save(key: string, value: any): Promise<void> {
+    console.log(key, value);
+  }
+
+  //public async recover<T>(key: string): Promise<T | null> {}
+
+  public async invalidate(key: string): Promise<void> {}
 }
