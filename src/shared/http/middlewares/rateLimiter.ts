f2074d23b5fbb7e2f97962051ece89c3a8915343
ivnogg <ivnocrypto@gmail.com>
2023-02-24 21:06:34 -0300

    Import AppError

--- src/shared/http/middlewares/rateLimiter.ts
+++ src/shared/http/middlewares/rateLimiter.ts
@@ -1,3 +1,4 @@
 import { Request, Response, NextFunction } from 'express';
 import redis from 'redis';
 import { RateLimiterRedis } from 'rate-limiter-flexible';
+import AppError from '@shared/errors/AppError';
