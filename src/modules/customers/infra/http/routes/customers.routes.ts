9c3a1bb193eeb73734fdb02e0887da4f9c8d68f6
ivnogg <ivnocrypto@gmail.com>
2023-03-03 11:10:59 -0300

    package.json corrigido caminho do server.ts

--- src/modules/customers/infra/http/routes/customers.routes.ts
+++ src/modules/customers/infra/http/routes/customers.routes.ts
@@ -1,4 +1,4 @@
-import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
+import isAuthenticated from '@shared/infra/http/middlewares/isAuthenticated';
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
 import CustomersController from '../controllers/CustomersController';
