fc01ab330aec7d946dc988b3934e2844b354a043
ivnogg <ivnocrypto@gmail.com>
2023-03-05 11:17:04 -0300

    metodo findByEmail adicionado em ICustomersRepository.ts

--- src/modules/customers/domain/repositories/ICustomersRepository.ts
+++ src/modules/customers/domain/repositories/ICustomersRepository.ts
@@ -3,4 +3,5 @@ import { ICustomer } from '../models/ICustomer';
 export interface ICustomersRepository {
   findByName(name: string): Promise<ICustomer | undefined>;
   findById(id: string): Promise<ICustomer | undefined>;
+  findByEmail(email: string): Promise<ICustomer | undefined>;
 }
