49edd72c6b7df6c25d997d0941f9b4c580de5f28
ivnogg <ivnocrypto@gmail.com>
2023-03-05 19:25:26 -0300

    criado const createCustomer = container.resolve(CreateCustomerService)

--- src/modules/customers/infra/http/controllers/CustomersController.ts
+++ src/modules/customers/infra/http/controllers/CustomersController.ts
@@ -14,7 +14,6 @@ export default class CustomersController {
 
     return response.json(customer);
   }
-
   public async show(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
 
