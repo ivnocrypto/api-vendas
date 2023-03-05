c72269a35b269fb950586015efe5aec7a8fc5f8d
ivnogg <ivnocrypto@gmail.com>
2023-03-05 11:43:56 -0300

    metodo save() adicionado em ICustomersRepository.ts

--- src/modules/customers/domain/repositories/ICustomersRepository.ts
+++ src/modules/customers/domain/repositories/ICustomersRepository.ts
@@ -6,4 +6,5 @@ export interface ICustomersRepository {
   findById(id: string): Promise<ICustomer | undefined>;
   findByEmail(email: string): Promise<ICustomer | undefined>;
   create(data: ICreateCustomer): Promise<ICustomer>;
+  save(customer: ICustomer): Promise<ICustomer>;
 }
