e40eaf1c0f95b6f55ba0f923af5ac73dd7a4cfb9
ivnogg <ivnocrypto@gmail.com>
2023-03-20 14:08:03 -0300

    criado repositorio fake de customers

--- src/modules/customers/domain/repositories/fakes/FakeCustomersRepository.ts
+++ src/modules/customers/domain/repositories/fakes/FakeCustomersRepository.ts
@@ -1,27 +1,45 @@
+import { v4 as uuidv4 } from 'uuid';
 import { ICreateCustomer } from '../../../../../modules/customers/domain/models/ICreateCustomer';
 import { ICustomersRepository } from '../../../../../modules/customers/domain/repositories/ICustomersRepository';
-import Customer from '../entities/Customer';
+import Customer from '../../../../../modules/customers/infra/typeorm/entities/Customer';
 
-class CustomersRepository implements ICustomersRepository {
-  private ormRepository: Repository<Customer>;
+class FakeCustomersRepository
+  implements Omit<ICustomersRepository, 'remove' | 'findAll'>
+{
+  private customers: Customer[] = [];
 
-  constructor() {
-    this.ormRepository = getRepository(Customer);
+  public async create({ name, email }: ICreateCustomer): Promise<Customer> {
+    const customer = new Customer();
+
+    customer.id = uuidv4();
+    customer.name = name;
+    customer.email = email;
+
+    this.customers.push(customer);
+
+    return customer;
   }
 
-  public async create({ name, email }: ICreateCustomer): Promise<Customer> {}
+  public async save(customer: Customer): Promise<Customer> {
+    Object.assign(this.customers, customer);
 
-  // public async save(customer: Customer): Promise<Customer> {}
+    return customer;
+  }
 
-  // public async remove(customer: Customer): Promise<void> {}
+  public async findByName(name: string): Promise<Customer | undefined> {
+    const customer = this.customers.find(customer => customer.name === name);
+    return customer;
+  }
 
-  // public async findAll(): Promise<Customer[] | undefined> {}
+  public async findById(id: string): Promise<Customer | undefined> {
+    const customer = this.customers.find(customer => customer.id === id);
+    return customer;
+  }
 
-  // public async findByName(name: string): Promise<Customer | undefined> {}
-
-  // public async findById(id: string): Promise<Customer | undefined> {}
-
-  // public async findByEmail(email: string): Promise<Customer | undefined> {}
+  public async findByEmail(email: string): Promise<Customer | undefined> {
+    const customer = this.customers.find(customer => customer.email === email);
+    return customer;
+  }
 }
 
-export default CustomersRepository;
+export default FakeCustomersRepository;
