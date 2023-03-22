f806841d27e72143002a5ef7ee0b7b0d6ccf3ab7
ivnogg <ivnocrypto@gmail.com>
2023-03-22 17:36:42 -0300

    importado CreateCustomerService e FakeCustomerService em CreateCustomerService.spec.ts

--- src/modules/customers/services/CreateCustomerService.spec.ts
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -1,9 +1,12 @@
+import CreateCustomerService from './CreateCustomerService';
+import FakeCustomersRepository from '../domain/repositories/fakes/FakeCustomersRepository';
+
 describe('CreateCustomer', () => {
   it('should be able to create a new customer', () => {
     expect(1).toBe(1);
   });
 
   it('should not be able to create two customers with the same email', () => {
-    expect(1).toBe(1);
+    expect(1).toBe(2);
   });
 });
