41c459567183f8420868392523f9ef011d4653a5
ivnogg <ivnocrypto@gmail.com>
2023-03-27 14:14:43 -0300

    definindo tipos em CreateCustomerService.spec.ts

--- src/modules/customers/services/CreateCustomerService.spec.ts
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -3,22 +3,25 @@ import CreateCustomerService from './CreateCustomerService';
 import FakeCustomersRepository from '../domain/repositories/fakes/FakeCustomersRepository';
 import AppError from '../../../shared/errors/AppError';
 
+let fakeCustomersRepository: FakeCustomersRepository;
+
 describe('CreateCustomer', () => {
+  beforeEach(() => {});
+
   it('should be able to create a new customer', async () => {
     const fakeCustomersRepository = new FakeCustomersRepository();
-
     const createCustomer = new CreateCustomerService(fakeCustomersRepository);
 
     const customer = await createCustomer.execute({
       name: 'Ivano GG',
       email: 'ivanoteste@teste.com',
     });
+
     expect(customer).toHaveProperty('id');
   });
 
   it('should not be able to create two customers with the same email', async () => {
     const fakeCustomersRepository = new FakeCustomersRepository();
-
     const createCustomer = new CreateCustomerService(fakeCustomersRepository);
 
     await createCustomer.execute({
