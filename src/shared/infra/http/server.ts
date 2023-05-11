08cf8d666cf38853011cc2e4426c0a9097c72a03
ivnogg <ivnocrypto@gmail.com>
2023-05-11 16:04:20 -0300

    refactor: src/shared/infra/app and server

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,51 +1,10 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-import cors from 'cors';
-import { errors } from 'celebrate';
-import { pagination } from 'typeorm-pagination';
-import routes from './routes';
-import AppError from '../../../shared/errors/AppError';
-import '../typeorm';
-import '../../container';
-import uploadConfig from '../../../config/upload';
-import rateLimiter from './middlewares/rateLimiter';
+import { app } from './app';
+import { dataSource } from '../typeorm';
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use(rateLimiter);
-
-app.use(pagination);
-
-app.use('/files', express.static(uploadConfig.directory));
-app.use(routes);
-
-app.use(errors());
-
-app.use(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: 'error',
-        message: error.message,
-      });
-    }
-    // eslint-disable-next-line no-console
-    console.log(error);
-
-    return response.status(500).json({
-      status: 'error',
-      message: 'Internal Server error',
-    });
-  },
-);
-
-app.listen(3333, () => {
-  // eslint-disable-next-line no-console
-  console.log('Server on port 3333!✨');
+dataSource.initialize().then(() => {
+  const server = app.listen(process.env.PORT || 3333, () => {
+    console.log(`Server started on port ${process.env.PORT || 3333}! 🏆`);
+  });
 });
